715ca1cd896a6290759e80bb86e284a03be6afee
Malek Jerbi <malek.jerbi@gmail.Com>
2024-01-29 15:07:23 -0500

    Create dns_client.py

---
+++ dns_client.py
@@ -0,0 +1,21 @@
+import sys
+import socket
+import struct
+
+
+
+if __name__ == '__main__':
+    hostname = sys.argv[1]
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    id = 42
+    flags = 0x0100
+    query = struct.pack("!HHHHHH", id, flags,1,0,0,0)
+    query += b''.join(len(p).to_bytes(1, 'big') + p.encode('ascii') for p in hostname.split('.')) + b'\x00'
+    query += struct.pack('!HH', 1, 1)
+    s.sendto(query, ('8.8.8.8', 53))
+    print('ok')
+
+    while True:
+        data, sender = s.recvfrom(4096)
+        print(data)
+        print(sender)
