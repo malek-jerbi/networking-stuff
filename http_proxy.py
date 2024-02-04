5a36dcb3e95939cb096ee7fb434e2638b312c49b
Malek Jerbi <malek.jerbi@gmail.Com>
2024-02-04 12:21:28 -0500

    Update http_proxy.py

--- http_proxy.py
+++ http_proxy.py
@@ -12,28 +12,34 @@ s.listen()
 print(f"listening to connections on port {PORT}")
 
 while True:
-    conn, addr = s.accept()
-    print(f"new connection from {addr}")
+    try:
+        conn, addr = s.accept()
+        print(f"new connection from {addr}")
 
-    data = conn.recv(4096)
+        data = conn.recv(4096)
 
 
-    upstream_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    upstream_sock.connect(("127.0.0.1", 9000))
-    print("Connected to 127.0.0.1")
-    print(f"sending {len(data)}B")
-    upstream_sock.send(data)
-    print("sent")
-
-    while True:
-        new_data = upstream_sock.recv(4096)
-        if not new_data:
-            break
-        print(f"received response: {len(new_data)}B")
-        print(f"now forwarding that response back to the first connection ({addr})")
-        conn.send(new_data)
+        upstream_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        upstream_sock.connect(("127.0.0.1", 9000))
+        print("Connected to 127.0.0.1")
+        print(f"sending {len(data)}B")
+        upstream_sock.send(data)
         print("sent")
 
-    upstream_sock.close()
-    conn.close()
+        while True:
+            new_data = upstream_sock.recv(4096)
+            if not new_data:
+                break
+            print(f"received response: {len(new_data)}B")
+            print(f"now forwarding that response back to the first connection ({addr})")
+            conn.send(new_data)
+            print("sent")
+    except ConnectionRefusedError:
+        conn.send(b"HTTP/1.1 502 Bad Gateway\r\n\r\n")
+        print("bad gateway")
+    except OSError as e:
+        print(e)
+    finally:
+        upstream_sock.close()
+        conn.close()
 s.close()
